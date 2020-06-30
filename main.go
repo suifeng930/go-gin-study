078dcc4d9b8a1131681322cab577b5e59356c79b
tom_glb <goodMorning_glb@atguigu.com>
2020-06-30 22:15:58 +0800

    go gin study  hello world  编写项目启动、路由文件

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/mahongcheng/go-gin-study/pkg/setting"
+	"github.com/mahongcheng/go-gin-study/routers"
 	"net/http"
 )
 
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	//1.返回 Gin 的type Engine struct{...}，里面包含RouterGroup，相当于创建一个路由Handlers，可以后期绑定各类的路由规则和函数、中间件等
-	router := gin.Default()
-	//2.创建不同的 HTTP 方法绑定到Handlers中，也支持 POST、PUT、DELETE、PATCH、OPTIONS、HEAD 等常用的 Restful 方法
-	router.GET("/test", func(c *gin.Context) {
-		//3.gin.H{…}：就是一个map[string]interface{}
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	//router := gin.Default()
+	////2.创建不同的 HTTP 方法绑定到Handlers中，也支持 POST、PUT、DELETE、PATCH、OPTIONS、HEAD 等常用的 Restful 方法
+	//router.GET("/test", func(c *gin.Context) {
+	//	//3.gin.H{…}：就是一个map[string]interface{}
+	//	c.JSON(200, gin.H{
+	//		"message": "test",
+	//	})
+	//})
 	//4.gin.Context：Context是gin中的上下文，它允许我们在中间件之间传递变量、管理流、验证 JSON 请求、响应 JSON 请求等，
 	// 在gin中包含大量Context的方法，例如我们常用的DefaultQuery、Query、DefaultPostForm、PostForm等等
 
@@ -32,6 +32,8 @@ func main() {
 	//MaxHeaderBytes：请求头的最大字节数
 	//ConnState：指定一个可选的回调函数，当客户端连接发生变化时调用
 	//ErrorLog：指定一个可选的日志记录器，用于接收程序的意外行为和底层系统错误；如果未设置或为nil则默认以日志包的标准日志记录器完成（也就是在控制台输出）
+
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
